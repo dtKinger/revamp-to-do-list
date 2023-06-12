ba84e75325584fa83255ddfa1e8c98ac63be88bb
Dan King <king.daniel.thomas@gmail.com>
2023-06-12 12:18:58 +0700

    Use class syntax to construct Task objects

--- src/new-task.js
+++ src/new-task.js
@@ -4,47 +4,52 @@ import { completedTasks } from "./to-dones.js";
 
 export const addToDo = document.getElementById('add-todo-btn'); // Add ToDo Button in Nav
 export const addNewForm = document.getElementById('add-new-to-do');
-export const submitNewToDo = document.getElementById('submit-new');
+// export const submitNewToDo = document.getElementById('submit-new');
 
-// export let title = document.getElementById('title')
-// export let description = document.getElementById('description');
-// export let priority = document.getElementById('priority-select');
-// export let dueDate = document.getElementById('date-picker');
-// export let createdAt = new Date();
 
-// class Task {
-//   constructor (title, description, priority, dueDate, createdAt){
-//     this.title = title.value;
-//     this.description = description.value;
-//     this.priority = priority.value;
-//     this.dueDate = dueDate.value;
-//     this.createdAt = createdAt;
-//   }  
-// }
 
-// submitNewToDo.addEventListener('click', newToDoObj);
-// export function newToDoObj (e) {
-//     e.preventDefault();
-//     let taskDetails = new Task;
-//     console.log(taskDetails);
-//     tasksToDo.push(taskDetails);
-//     addNewForm.reset();
-//     renderToDos();
-// }
+class Task {
+  constructor (title, description, priority, dueDate, createdAt){
+    this.title = title;
+    this.description = description;
+    this.priority = priority;
+    this.dueDate = dueDate;
+    this.createdAt = createdAt;
+  }  
+}
 
 addNewForm.addEventListener('submit', newToDoObj);
-
 export function newToDoObj (e) {
-  e.preventDefault();
-  let myFormData = new FormData(e.target);
-  let formDataObj = {};
-  myFormData.forEach((value, key) => (formDataObj[key] = value));
-  let formDataObjWithDate = {...formDataObj, "createdAt": new Date().toDateString()} // Add meta data of the date created.
-  // sortAll();
-  tasksToDo[tasksToDo.length] = formDataObjWithDate;
-  addNewForm.reset();
-  renderToDos();
-};
+    e.preventDefault();
+
+    //capture input values.
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let priority = document.getElementById('priority-select').value;
+    let dueDate = document.getElementById('date-picker').value;
+    let createdAt = new Date();
+
+    let taskDetails = new Task(title, description, priority, dueDate, createdAt)
+
+    console.log(taskDetails);
+    tasksToDo.push(taskDetails);
+    addNewForm.reset();
+    renderToDos();
+}
+
+// addNewForm.addEventListener('submit', newToDoObj);
+
+// export function newToDoObj (e) {
+//   e.preventDefault();
+//   let myFormData = new FormData(e.target);
+//   let formDataObj = {};
+//   myFormData.forEach((value, key) => (formDataObj[key] = value));
+//   let formDataObjWithDate = {...formDataObj, "createdAt": new Date().toDateString()} // Add meta data of the date created.
+//   // sortAll();
+//   tasksToDo[tasksToDo.length] = formDataObjWithDate;
+//   addNewForm.reset();
+//   renderToDos();
+// };
 
 addToDo.addEventListener('click', () => {
   if (!addNewForm.classList.contains('show')){
